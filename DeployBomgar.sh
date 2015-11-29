c2e66a4fcaa709b5d00b90bc8e20778882847eac
mikemuir <mike@creativeresources.net>
2015-11-29 10:48:16 -0800

    Download and install from AWS
    Download custom installer from AWS, uncompress file and remove it, then
    trigger the install.

--- DeployBomgar.sh
+++ DeployBomgar.sh
@@ -3,7 +3,18 @@
 #Created by Mike Muir for CRTG 11/29/15
 
 ##Deploy Bomgar
-##Initially used by generating a bomgar jump client, copying unique client to /Users/Shared and editing this script w/ .dmg name.
-hdiutil attach -mountpoint /Volumes/foobar /Users/Shared/bomgar-scc-[uniqueID].dmg
+
+##Initially used by generating a bomgar jump client, uploading the compressed installer
+##to AWS.
+#First download compressed Jump Client from CRTG AWS Instance:
+/usr/bin/curl http://web.crtg.io/base/bomgarinstallers/TestDeploy/bomgar-scc-w0idc30efz6iiwfhfeii1gj8y8g1fzw11i685zhc40jc90.dmg.zip > /tmp/bomgar.zip
+#Next, uncompress downloaded jump client:
+unzip -d /tmp /tmp/bomgar.zip
+rm -rf /tmp/__MACOSX
+rm /tmp/bomgar.zip
+
+#Next, mount the downloaded and uncompressed .dmg:
+hdiutil attach -mountpoint /Volumes/foobar /tmp/bomgar-scc-*
  
+ #...and run the bomgar installer
 '/Volumes/foobar/Double-Click To Start Support Session.app/Contents/MacOS/sdcust'
