d7a0d5f8c186d7dbd5bc41b1db4f3a8ade1d0e39
mikemuir <mike@creativeresources.net>
2015-12-01 15:05:03 -0800

    Define bomgarURL variable
    Changes to use a predefined variable.

--- DeployBomgar.sh
+++ DeployBomgar.sh
@@ -3,11 +3,12 @@
 #Created by Mike Muir for CRTG 11/29/15
 
 ##Deploy Bomgar
+bomgarURL= "http://web.crtg.io/base/bomgarinstallers/TestDeploy/bomgar-scc-w0idc30efz6iiwfhfeii1gj8y8g1fzw11i685zhc40jc90.dmg.zip" 
 
 ##Initially used by generating a bomgar jump client, uploading the compressed installer
 ##to AWS.
 #First download compressed Jump Client from CRTG AWS Instance:
-/usr/bin/curl http://web.crtg.io/base/bomgarinstallers/TestDeploy/bomgar-scc-w0idc30efz6iiwfhfeii1gj8y8g1fzw11i685zhc40jc90.dmg.zip > /tmp/bomgar.zip
+/usr/bin/curl $bomgarURL > /tmp/bomgar.zip
 #Next, uncompress downloaded jump client:
 unzip -d /tmp /tmp/bomgar.zip
 rm -rf /tmp/__MACOSX
